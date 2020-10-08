4155c8392aafe5ea9af22ca9c0c1ef711966be20
namwoo.kim <sky_9233@naver.com>
2020-10-09 00:24:55 +0900

    DAY1

--- DAY001_[프로그래머스]_전화번호목록.py
+++ DAY001_[프로그래머스]_전화번호목록.py
@@ -1,8 +1,8 @@
 def solution(phone_book):
     
     for i in range(len(phone_book)):
-        for j in range(i+1, len(phone_book)):
-            if phone_book[i] == phone_book[j][:len(phone_book[i])]:
+        for j in range(len(phone_book)):
+            if j!=i and phone_book[i] == phone_book[j][:len(phone_book[i])]:
                 return False
             
     return True
