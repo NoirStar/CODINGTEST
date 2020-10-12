c06ffd9bdc4b2fdc0498867cf551f755721294ca
namwoo.kim <sky_9233@naver.com>
2020-10-12 22:11:04 +0900

    DAY5

--- DAY004_[프로그래머스]_베스트엘범.js
+++ DAY004_[프로그래머스]_베스트엘범.js
@@ -1,6 +1,8 @@
 function solution(genres, plays) {
     let answer = []
     let music = {}
+    let musicArray = []
+    
     music=genres.reduce((acc,cur,index) => {
         if(acc[cur]) {
             acc[cur].push([index,plays[index]])
@@ -10,6 +12,20 @@ function solution(genres, plays) {
             return acc
         }
     },{})
-    console.log(music)
+    musicArray = Object.values(music).map(v => {
+        v.push(v.reduce((acc,cur) => (acc=acc+parseInt(cur[1]),acc),0))
+        return v.sort((a,b) => b[1] - a[1])
+    }).sort((a,b) =>  b[b.length - 1] - a[a.length - 1])
+    
+    for(let i of musicArray) {
+        if(i.length > 2) {
+            for(let j=0;j<2;j++) {
+                answer.push(i[j][0])
+            }
+        } else {
+            answer.push(i[0][0])
+        }
+    }    
+    
     return answer
 }
