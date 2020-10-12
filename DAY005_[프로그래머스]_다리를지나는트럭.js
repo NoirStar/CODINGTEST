fb860558b7e5334050ba8d9685416699dce6c3fb
namwoo.kim <sky_9233@naver.com>
2020-10-13 01:53:48 +0900

    DAY5

--- DAY005_[프로그래머스]_다리를지나는트럭.js
+++ DAY005_[프로그래머스]_다리를지나는트럭.js
@@ -1,4 +1,18 @@
 function solution(bridge_length, weight, truck_weights) {
-    var answer = 0;
-    return answer;
+    let answer = 0
+    let bridge = Array.from({length: bridge_length}, () => 0);
+    
+    do {  
+        if(truck_weights[0] + bridge.slice(1).reduce((acc,cur) => (acc=acc+cur,acc),0) <= weight) {
+            bridge.push(truck_weights.shift())
+            bridge.shift()
+            answer++
+        } else {
+            bridge.push(0)
+            bridge.shift()
+            answer++
+        }
+        
+    } while((truck_weights.length > 0) || (bridge.reduce((acc,cur) => (acc=acc+cur,acc),0) > 0))
+    return answer
 }
