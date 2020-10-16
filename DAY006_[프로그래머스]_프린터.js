62a067f8cce47483d665f13b77ed1a84ff0a5c14
namwoo.kim <sky_9233@naver.com>
2020-10-16 14:18:40 +0900

    DAY6

--- DAY006_[프로그래머스]_프린터.js
+++ DAY006_[프로그래머스]_프린터.js
@@ -1,17 +1,20 @@
 function solution(priorities, location) {
     let answer = 0
-    let second = 0
     
-    if(Math.max(...priorities) == priorities[location]) return 1
-    else {
-        second = [...new Set(priorities)].sort()[1]
-        priorities=priorities.map(v => {
-            if(v > second) {
-                answer++
-                return 0
-            } else return v
-        })
+    while(1) {
+        // 우선순위 더 높은게 있어서 인쇄 못함
+        if(priorities.find(v => v > priorities[0]) > 0){
+            priorities.push(priorities.shift())
+            if(--location == -1) location = priorities.length -1
+            
+        } else {
+            // 인쇄
+            priorities.shift()
+            answer++
+            if(--location == -1) break
+        }
     }
-    console.log(priorities)
+
+   
     return answer
 }
