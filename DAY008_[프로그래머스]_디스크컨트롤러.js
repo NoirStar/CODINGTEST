27673473719365281dc142162a3c20f02fea477b
namwoo.kim <sky_9233@naver.com>
2020-10-20 23:46:37 +0900

    DAY8

--- DAY008_[프로그래머스]_디스크컨트롤러.js
+++ DAY008_[프로그래머스]_디스크컨트롤러.js
@@ -1,49 +1,33 @@
-// min heap
-// n: parent , 2*n+1 : left child, 2*n+2 : right child
 
-
-class Heap {
-    constructor() {
-        this.heap = []
-    }
-    swap(a, b) {
-        [this.heap[a], this.heap[b]] = [this.heap[b] , this.heap[a]]
-    }
-    size() {
-        return this.heap.length
-    }
-    print() {
-        console.log(this.heap)
-    }
-    push(value) {
-        let idx, parent = 0
-        this.heap.push(value)
-        idx = this.heap.length-1
-        parent = Math.floor((idx-1)/2)
-
-        while(this.heap[parent][1] > value[1]) {
-            this.swap(idx, parent)
+function solution(jobs) {
+    let answer, firstJob = 0
+    let total = []
+    let heap = []
+    let idx, parent = 0
+    
+    
+    firstJob = jobs.shift()
+    jobs.map(v => {
+        heap.push(v)
+        idx = heap.length-1
+        parent = idx == 0 ? 0 : Math.floor((idx-1)/2)
+        
+        console.log(v[1])
+        while(heap[parent][1] > v[1]) {
+            
+            [heap[parent],heap[idx]] = [heap[idx], heap[parent]]
             idx = parent
             parent = Math.floor((idx-1)/2)
         }
-        return this.print()
-    }
-}
-
-
-function solution(jobs) {
-    let answer = 0
-    let total = []
-    let heap = []
+    })
+    heap.unshift(firstJob)
     
-    jobs.sort((a,b) => a[1] - b[1])
-    jobs.reduce((acc,cur) => {
+    heap.reduce((acc,cur) => {
         acc = acc+cur[1]
         total.push(acc-cur[0])
         return acc
     },0)
     
-    console.log(total)
     
     
     return Math.floor(total.reduce((acc,cur) => (acc=acc+cur, acc)) / total.length)
