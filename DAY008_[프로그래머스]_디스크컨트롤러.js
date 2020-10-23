3ca5fd0edfe4de28c15154df5ac44d2b8330751f
namwoo.kim <sky_9233@naver.com>
2020-10-23 15:36:20 +0900

    DAY11

--- DAY008_[프로그래머스]_디스크컨트롤러.js
+++ DAY008_[프로그래머스]_디스크컨트롤러.js
@@ -1,34 +1,25 @@
-
-function solution(jobs) {
-    let answer, firstJob = 0
-    let total = []
-    let heap = []
-    let idx, parent = 0
+const solution = (jobs) => {
+    let answer,time,j = 0;
+    const priorityQueue = [];
     
+    jobs.sort((a,b) => a[0] - b[0]);
     
-    firstJob = jobs.shift()
-    jobs.map(v => {
-        heap.push(v)
-        idx = heap.length-1
-        parent = idx == 0 ? 0 : Math.floor((idx-1)/2)
-        
-        console.log(v[1])
-        while(heap[parent][1] > v[1]) {
-            
-            [heap[parent],heap[idx]] = [heap[idx], heap[parent]]
-            idx = parent
-            parent = Math.floor((idx-1)/2)
+    // j는 jobs를 도는 index 인거같음.
+    while(j < jobs.length || priorityQueue.length !== 0) {
+        // 현재 시간이 요청되는 jobs 보다 같거나 크면 (앞의 job이 수행되는 동안 우선순위 큐에 넣어둠)
+        if(jobs.length > j && time >= jobs[j][0]) {
+            priorityQueue.push(jobs[j++]);
+            priorityQueue.sort((a,b) => a[1] - b[1]);
+            continue;
         }
-    })
-    heap.unshift(firstJob)
-    
-    heap.reduce((acc,cur) => {
-        acc = acc+cur[1]
-        total.push(acc-cur[0])
-        return acc
-    },0)
-    
-    
-    
-    return Math.floor(total.reduce((acc,cur) => (acc=acc+cur, acc)) / total.length)
-}
+        // priorityQueue 안에 있는 작업들 하나씩 처리
+        if(priorityQueue.length !== 0) {
+            time += priorityQueue[0][1];
+            answer += time - priorityQueue[0][0];
+            priorityQueue.shift();
+        } else {
+            time = jobs[j][0];
+        }
+    }
+    return parseInt(answer / jobs.length);
+};
