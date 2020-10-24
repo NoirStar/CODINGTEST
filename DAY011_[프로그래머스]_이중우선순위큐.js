5c2d3d47691b5083bed4a459e688dedd2a4af7af
namwoo.kim <sky_9233@naver.com>
2020-10-24 18:20:43 +0900

    DAY11

--- DAY011_[프로그래머스]_이중우선순위큐.js
+++ DAY011_[프로그래머스]_이중우선순위큐.js
@@ -1,13 +1,13 @@
 const solution = (operations) => {
-    let answer = [];
     let queue = [];
     
     operations.map(operation => {
-        if(operation[0] === 'D') {
+        if(operation[0] === 'I') {
             queue.push(operation.substring(2));
+            queue.sort((a,b) => parseInt(a) - parseInt(b));
         } else {
-            
+            operation[2] === '-' ? queue.shift() : queue.pop()
         }
     });
-    return answer;
+    return queue.length > 0 ? [Math.max(...queue), Math.min(...queue)] : [0,0];
 };
