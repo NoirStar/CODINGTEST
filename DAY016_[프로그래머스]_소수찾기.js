e9591918d5f8cf08a0356b34fb94f29be4bd0afd
namwoo.kim <sky_9233@naver.com>
2020-10-27 16:10:12 +0900

    DAY16

--- DAY016_[프로그래머스]_소수찾기.js
+++ DAY016_[프로그래머스]_소수찾기.js
@@ -1,13 +1,19 @@
-// 소수란 1과 자신외 약수를 가지지 않는 수
 const solution = (numbers) => {
     let answer = 0;
     
-
+    // 1 2 3  12 13 21 23 31 32 123 213 312
+    numbers.split('').reduce((acc,cur) => {
+        for(let i=0;i<numbers.length;i++) {
+            
+        }
+    },[])
     return answer;
 }
 
+
 const isPrime = (number) => {
+    if(number == 1) return false;
     for(let i=2; i<Math.sqrt(number);i++) {
-        return number % i == 0 ? false : true
+        return number % i == 0 ? false : true;
     }
 }
