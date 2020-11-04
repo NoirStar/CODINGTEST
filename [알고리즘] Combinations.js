d416cfcb488aff0f25306dd6cff9922549ef4e0c
namwoo.kim <sky_9233@naver.com>
2020-11-04 13:57:07 +0900

    algo

--- [알고리즘] Combinations.js
+++ [알고리즘] Combinations.js
@@ -1,5 +1,6 @@
 // https://medium.com/@jun.choi.4928/javascript%EB%A1%9C-%EC%88%9C%EC%97%B4%EA%B3%BC-%EC%A1%B0%ED%95%A9-%EC%95%8C%EA%B3%A0%EB%A6%AC%EC%A6%98-%EA%B5%AC%ED%98%84%ED%95%98%EA%B8%B0-21df4b536349
-
+// nCr = nCn-r
+// nCr = n-1Cr + n-1Cr-1  하나를 고정시키고 돌릴때
 const getCombinations = (arr, selectNumber) => {
     const results = [];
     // 배열에서 한개씩 택할 때, 모든배열의 원소를 리턴. ex) 3C1
---
+++ [알고리즘] Permutaions.js
@@ -0,0 +1,2 @@
+// https://medium.com/@jun.choi.4928/javascript%EB%A1%9C-%EC%88%9C%EC%97%B4%EA%B3%BC-%EC%A1%B0%ED%95%A9-%EC%95%8C%EA%B3%A0%EB%A6%AC%EC%A6%98-%EA%B5%AC%ED%98%84%ED%95%98%EA%B8%B0-21df4b536349
+
