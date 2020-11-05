f6151e5276a36c2ce6c676bd8b7948c34915f5b1
namwoo.kim <sky_9233@naver.com>
2020-11-05 22:34:36 +0900

    DAY16

--- DAY016_[프로그래머스]_소수찾기.js
+++ DAY016_[프로그래머스]_소수찾기.js
@@ -1,17 +1,35 @@
 const solution = (numbers) => {
-    let answer = 0;
-    
-    numbers.split('').reduce((acc,cur) => {
-        for(let i=0;i<numbers.length;i++) {
-            
-        }
-    },[])
-    return answer;
+    let answer = [];
+
+    for(let i=1; i<numbers.length+1;i++){
+        getPermutations([...numbers],i).forEach(v => isPrime(parseInt(v)) ? answer.push(parseInt(v)) : answer);
+    }
+
+    console.log([...new Set(answer)])
+    return [...new Set(answer)].length;
 }
 
+
 const isPrime = (number) => {
-    if(number == 1) return false;
-    for(let i=2; i<Math.sqrt(number);i++) {
-        return number % i == 0 ? false : true;
+    if(number <= 1) return false;
+    if(number == 2) return true;
+    for(let i=2; i<=Math.ceil(Math.sqrt(number));i++) {
+        if(number % i === 0) return false;
     }
+    return true;
 }
+
+const getPermutations = (arr, selectNumber) => {
+    const results = [];
+    if(selectNumber === 1) return arr.map((value) => value); // 1개씩 택할 때, 모든배열 리턴
+
+    arr.forEach((fixed, index, origin) => {
+        // 해당하는 fixed를 제외한 나머지 배열
+        const rest = [...origin.slice(0, index), ...origin.slice(index+1)];
+        const permutations = getPermutations(rest, selectNumber -1 );
+        const attached = permutations.map((permutation) => [fixed, ...permutation].join(''));
+        results.push(...attached);
+    });
+
+    return results;
+};
--- [알고리즘] Permutaions.js
+++ [알고리즘] Permutaions.js
@@ -1,2 +1,26 @@
 // https://medium.com/@jun.choi.4928/javascript%EB%A1%9C-%EC%88%9C%EC%97%B4%EA%B3%BC-%EC%A1%B0%ED%95%A9-%EC%95%8C%EA%B3%A0%EB%A6%AC%EC%A6%98-%EA%B5%AC%ED%98%84%ED%95%98%EA%B8%B0-21df4b536349
 
+// 조합
+// nCr = nCn-r
+// nCr = n-1Cr + n-1Cr-1  하나를 고정시키고 돌릴때
+
+// 순열
+// nPr = n * n-1Pr-1 = n-1Pr + r*n-1Pr-1
+
+// nPr = nCr * r!
+
+
+const getPermutations = (arr, selectNumber) => {
+    const results = [];
+    if(selectNumber === 1) return arr.map((value) => [value]); // 1개씩 택할 때, 모든배열 리턴
+    
+    arr.forEach((fixed, index, origin) => {
+        // 해당하는 fixed를 제외한 나머지 배열
+        const rest = [...origin.slice(0, index), ...origin.slice(index+1)];
+        const permutations = getPermutations(rest, selectNumber -1 );
+        const attached = permutations.map((permutation) => [fixed, ...permutation]);
+        results.push(...attached);
+    });
+
+    return results;
+};
