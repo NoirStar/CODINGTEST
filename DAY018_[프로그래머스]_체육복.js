8a2b0710d174729689eb2075295d95166680e67a
namwoo.kim <sky_9233@naver.com>
2020-11-09 19:28:07 +0900

    DAY18

--- DAY018_[프로그래머스]_체육복.js
+++ DAY018_[프로그래머스]_체육복.js
@@ -1,27 +1,25 @@
 const solution = (n, lost, reverse) => {
     let answer = n - lost.length;
     
-    lost.forEach((lostStudent,idx) => {
-        reverse = reverse.filter(reverseStudent => {
-            if(reverseStudent === lostStudent) {
-                lost.splice(idx,1);
-                answer++;
-            } else return reverseStudent;
-        });
-    });
-
-    lost.forEach(lostStudent => {
-        let idx = 0;
-        for(let reverseStudent of reverse) {
-            if(lostStudent-reverseStudent === 1 || lostStudent-reverseStudent === -1) {
-                reverse.splice(idx,1);
-                answer++;
-                break;
-            }
-            idx++;
+    // 여벌이 있고, 체육복을 도난당한 경우
+    lost = lost.filter((lostStudent) => {
+        let revIdx = reverse.findIndex(reverseStudent => reverseStudent === lostStudent);
+        if(revIdx === -1) return lostStudent
+        else {
+            reverse.splice(revIdx,1);
+            answer++;
         }
-        
-        
     });
+    
+    
+    // 여벌이 없고, 체육복 도난당한경우, 자기보다 앞번호 친구에게 우선순위로 빌려야 최대 값
+    lost.forEach(lostStudent => {
+        let revIdx = reverse.findIndex(reverseStudent => lostStudent-reverseStudent == 1 || lostStudent-reverseStudent == -1);
+        if(revIdx !== -1) {
+            reverse.splice(revIdx,1);
+            answer++;
+        }
+    });
+    
     return answer;
 };
