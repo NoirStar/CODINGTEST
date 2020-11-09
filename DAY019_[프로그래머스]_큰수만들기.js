3c26cbda78f31fe61b879920ab24341bb05cd5fc
namwoo.kim <sky_9233@naver.com>
2020-11-10 00:30:51 +0900

    DAY19

---
+++ DAY019_[프로그래머스]_큰수만들기.js
@@ -0,0 +1,20 @@
+const solution = (number, k) => getCombinations([...number], number.length - k).sort((a,b) => b-a)[0];
+
+
+
+const getCombinations = (arr, selectNumber) => {
+    let results = [];
+    if(selectNumber === 1) return arr.map((value) => value);
+    
+    arr.forEach((fixed, index, origin) => {
+        const rest = origin.slice(index+1);
+        const combinations = getCombinations(rest, selectNumber-1);
+        const attached = combinations.map((combination) => [fixed, ...combination].join(''));
+        
+        if(attached.length > 0)
+            console.log(attached)
+        //results = [Math.max(...results)];
+        results.push(...attached);
+    });
+    return results;
+};
