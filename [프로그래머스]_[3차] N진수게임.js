0caf86ca28bcf754b8134d87ee963438162d5b69
namwoo.kim <sky_9233@naver.com>
2021-07-16 00:20:23 +0900

    ct

--- [프로그래머스]_[3차] N진수게임.js
+++ [프로그래머스]_[3차] N진수게임.js
@@ -1,26 +1,16 @@
-const solution = (msg) => {
-    let dict = [];
-    let answer = [];
-    let findLen = 1;
-    for(let i=65;i<91;i++) dict.push(String.fromCharCode(i));
+const solution = (n,t,m,p) => {
     
-    while(msg.length > 0) {
-        // 못찾았을때
-        if(dict.find(e => e === msg.substr(0,findLen)) === undefined) {
-            answer.push(dict.findIndex(e => e === msg.substr(0,findLen-1)) + 1);
-            dict.push(msg.substr(0,findLen));
-            msg = msg.length === msg.substr(findLen-1).length ? '' : msg.substr(findLen-1);
-            findLen = 1;
-        // 찾았을 떄
-        } else {
-            if(msg.length === findLen) {
-                answer.push(dict.findIndex(e => e === msg) + 1);
-                msg = '';
-            } else {
-                findLen++;
-            }
-        }
+    let number = '';
+    let answer ='';
+    
+    for(let i=0; number.length < m*p*t ; i++) {
+        number += i.toString(n);
     }
-    return answer;
     
+    number = number.toUpperCase().split('')
+    
+    for(let i=p; i<number.length; i=i+m) {
+        answer += number[i-1];
+    }
+    return answer.substr(0,t);
 }
