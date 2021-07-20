b1cef519dc272e18f484d2ebcf6a38519f7937f1
namwoo.kim <sky_9233@naver.com>
2021-07-20 22:13:20 +0900

    add

---
+++ [프로그래머스]_[3차] 방금그곡.js
@@ -0,0 +1,16 @@
+const solution = (m, musicInfos) => {
+    let answer = '';
+    
+    musicInfos = musicInfos.map(e => {
+        let eArr = e.split(',');
+        let timeDiff = (eArr[1].split(':')[0] - eArr[0].split(':')[0])*60 + 
+            (eArr[1].split(':')[1] - eArr[0].split(':')[1]);
+        return `${timeDiff},${eArr[2]},${eArr[3].repeat(Math.ceil(timeDiff / eArr[3].length))}`;
+    });
+    
+    
+    console.log(musicInfos)
+    answer = musicInfos.filter(e => e.indexOf(m) != -1);
+    
+    return answer.length == 0 ? '(None)' : answer[0].split(',')[1];
+}
