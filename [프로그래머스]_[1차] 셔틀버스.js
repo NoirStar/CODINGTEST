43573fb74f1d6528e8609088aa0f09a9536bfe50
namwoo.kim <sky_9233@naver.com>
2021-07-28 05:58:09 +0900

    ct

--- [프로그래머스]_[1차] 셔틀버스.js
+++ [프로그래머스]_[1차] 셔틀버스.js
@@ -1,39 +1,23 @@
-const solution = (str1, str2) => {
-    let answer = 0;
-    let intersection,union
-    str1 = stringToArray(str1);
-    str2 = stringToArray(str2);
-    union = [...str1, ...str2];
+function solution(n, t, m, timetable) {
+    let answer = '';
+    let busTimetable = ['09:00'];
+    
+    timetable.sort((a,b) => {
+       return new Date(`1970-01-01 ${a}`) - new Date(`1970-01-01 ${b}`);
+    });
+    
+    
+    for(let i=1; i<n; i++) {
+        let time = new Date(`1970-01-01 09:00`);
+        time = new Date(time.getTime() + i*t*60000)
+        busTimetable.push(time.toTimeString().substr(0,5))
+    }
+    
 
-    intersection = str1.reduce((acc,cur) => {
-        const idx = str2.findIndex(e => e === cur);
-        if(idx !== -1) {
-            str2.splice(idx,1);
-            acc.push(cur);
-            return acc;
-        } 
-        return acc;
-    },[])
-
-   intersection.forEach(cur => {
-       const idx = union.findIndex(e => e === cur);
-       if(idx !== -1) {
-           union.splice(idx,1);
-       }
-   })
-
-    return intersection.length == 0 && union.length == 0 ? 
-        65536 : Math.floor(intersection.length / union.length * 65536);
-}
-
-
-function stringToArray(str) {
-    return [...str].reduce((acc,cur,idx,arr) => {
-        if(arr.length-1 !== idx && /^[a-z]{2}$/i.test(arr[idx]+arr[idx+1])) {
-            acc.push((arr[idx]+arr[idx+1]).toUpperCase());
-            return acc;
-        } else {
-            return acc;
-        }
-    },[]);
+    
+    console.log(busTimetable)
+    
+    
+    
+    return answer;
 }
